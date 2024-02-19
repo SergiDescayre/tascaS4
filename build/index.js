f07a1326c009bd25c2c39556c8290a21a8024c76
Sergi Descayre <lepetitdesca@gmail.com>
2024-02-19 13:36:45 +0100

    exercici 3 i 4

--- build/index.js
+++ build/index.js
@@ -10,12 +10,48 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 const nextJoke = document.querySelector(".next-joke");
 const printJoke = document.querySelector(".joke");
+const textWeather = document.querySelector(".weather span");
+const iconWeather = document.querySelector(".weather img");
+console.log(iconWeather);
+let youVoted = false;
+const reportAcudits = {
+    joke: "",
+    score: 0,
+    date: ""
+};
+let reportJokes = [];
+const getWheater = () => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield fetch("http://api.weatherapi.com/v1/current.json?key=7bc335c15ed64584a45120452241902&q=barcelona&aqi=no");
+    const data = yield response.json();
+    textWeather.textContent = data.location.name;
+    iconWeather.setAttribute("src", data.current.condition.icon);
+    console.log(data.location.name);
+});
+const getScore = (num) => {
+    youVoted = true;
+    let date = new Date;
+    reportAcudits.score = num;
+    reportAcudits.date = date.toISOString();
+};
 const getJoke = () => __awaiter(void 0, void 0, void 0, function* () {
+    getWheater();
     const result = yield fetch("https://icanhazdadjoke.com/slack");
     const data = yield result.json();
     const joke = data.attachments[0].fallback;
     printJoke.innerHTML = joke;
-    console.log(data);
+    reportAcudits.joke = joke;
+    youVoted = false;
 });
 getJoke();
-nextJoke.addEventListener('click', getJoke);
+const saveAndNaextJoke = () => {
+    if (!youVoted) {
+        reportAcudits.score = 0;
+        reportJokes.push(Object.assign({}, reportAcudits));
+    }
+    else {
+        reportJokes.push(Object.assign({}, reportAcudits));
+    }
+    getJoke();
+    console.log(reportJokes);
+};
+nextJoke.addEventListener('click', saveAndNaextJoke);
--- index.ts
+++ index.ts
@@ -2,26 +2,68 @@
 const nextJoke = document.querySelector(".next-joke") as HTMLButtonElement
 const printJoke = document.querySelector(".joke") as HTMLParagraphElement
 
-interface Response {
-    attachments: ResponseAttachments[],
-    response_type: String,
-    username: String
-    }
+const textWeather = document.querySelector(".weather span") as HTMLHtmlElement
+const iconWeather = document.querySelector(".weather img") as HTMLHtmlElement
+console.log(iconWeather)
 
-interface ResponseAttachments{
-    fallback: Number,
-    footer: String,
-    text: String
+let youVoted: boolean = false
+
+const reportAcudits: {
+    joke: string,
+    score: number,
+    date: string
+} = {
+    joke : "",
+    score : 0,
+    date : ""
+}
+
+let reportJokes: { joke: string; score: number; date: string }[] = []
+
+const getWheater = async () => {
+    const response = await fetch("http://api.weatherapi.com/v1/current.json?key=7bc335c15ed64584a45120452241902&q=barcelona&aqi=no")
+    const data = await response.json()
+    textWeather.textContent=data.location.name
+    iconWeather.setAttribute("src", data.current.condition.icon)
+    console.log(data.location.name)
+
+}
+
+const getScore = (num: number) => {
+    youVoted = true
+    let date = new Date
+    reportAcudits.score = num
+    reportAcudits.date = date.toISOString()
+  
 }
 
 const getJoke = async () => {
-    const result:Response = await fetch("https://icanhazdadjoke.com/slack")
+    getWheater()
+    const result = await fetch("https://icanhazdadjoke.com/slack")
     const data = await result.json()
     const joke:string = data.attachments[0].fallback
     printJoke.innerHTML=joke
-    console.log(data)
+    reportAcudits.joke = joke
+    youVoted = false
+  
+}
+
+getJoke()
+
+const saveAndNaextJoke = () => {
+    if(!youVoted){
+        reportAcudits.score = 0
+        reportJokes.push({...reportAcudits})
+    }else{
+        reportJokes.push({...reportAcudits})
+    }
+    getJoke()
+    console.log(reportJokes)
 }
 
 
-getJoke()
-nextJoke.addEventListener('click',getJoke)
+
+
+
+
+nextJoke.addEventListener('click',saveAndNaextJoke)
